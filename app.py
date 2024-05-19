89179b1807668753d11263890612ae924bc8d7de
waleedali <wa318536497@gmail.com>
2024-05-19 13:08:47 +0300

    fixed code to handle a mongodb within a ec2

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ from flask import Flask, render_template, request
 import re
 import boto3
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 s3 = boto3.client('s3')
-
+client = MongoClient("mongo:27017")
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
-# Accessing the MongoDB collection
-collection = mongo.db.users
-
+db = client.project_db
+users_collection = db.users
 
 def is_valid_email(email):
     # Regular expression pattern for valid email address
@@ -29,7 +29,7 @@ def index():
         email = request.form["email"]
         if is_valid_email(email):
             user_data = {"name": name, "email": email}
-            collection.insert_one(user_data)
+            users_collection.insert_one(user_data)
             image_uri = "https://flask-aws-bucket.s3.eu-central-1.amazonaws.com/surf.jpg"
             return render_template("hello.html", name=name, image_url=image_uri)
     return render_template("homepage.html")
